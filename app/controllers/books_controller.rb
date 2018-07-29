90c8ff33cba67031ae1cf45ab49022ac4c4d5e96
MX <mxselleck@gmail.com>
2018-07-28 20:20:36 -0500

    remove include add query

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,14 @@
 class BooksController < ApplicationController
 before_action :set_book, only: [:show, :edit, :update, :destroy]
-  include DisplayBookContracts
+  # include DisplayBookContracts
   # include PdfFontsHelper
 
   # GET /books
   # GET /books.json
   def index
     @books = Book.all
-    # @layouts = PageLayout.all
-    # @contracts = Contract.all
+    @layouts = PageLayout.all
+    @contracts = Contract.all
     # if(params[:id] != nil)
     #   @contracts = Contract.where({ book_id: params[:id] })
     # else
