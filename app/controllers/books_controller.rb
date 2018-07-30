d27349b4b5721070d2fc0afda07295c03721c5f9
MX <mxselleck@gmail.com>
2018-07-29 20:27:25 -0500

    re add

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -73,25 +73,25 @@ helpers.this_method
   # PATCH/PUT /books/1
   # PATCH/PUT /books/1.json
   def update
-    # respond_to do |format|
-    #   if @book.update(book_params)
-    #     format.html { redirect_to @book, notice: 'Book was successfully updated.' }
-    #     format.json { render :show, status: :ok, location: @book }
-    #   else
-    #     format.html { render :edit }
-    #     format.json { render json: @book.errors, status: :unprocessable_entity }
-    #   end
-    # end
+    respond_to do |format|
+      if @book.update(book_params)
+        format.html { redirect_to @book, notice: 'Book was successfully updated.' }
+        format.json { render :show, status: :ok, location: @book }
+      else
+        format.html { render :edit }
+        format.json { render json: @book.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   # DELETE /books/1
   # DELETE /books/1.json
   def destroy
-    # @book.destroy
-    # respond_to do |format|
-    #   format.html { redirect_to books_url, notice: 'Book was successfully destroyed.' }
-    #   format.json { head :no_content }
-    # end
+    @book.destroy
+    respond_to do |format|
+      format.html { redirect_to books_url, notice: 'Book was successfully destroyed.' }
+      format.json { head :no_content }
+    end
   end
 
   private
