7794fd22551b226505f0bd97228ce6b66a9e630d
MX <mxselleck@gmail.com>
2018-07-31 23:23:02 -0500

    re-order jquery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,13 +10,15 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
-//= require activestorage
 //= require turbolinks
-//= require jquery3
-//= require bootstrap
+//= require activestorage
 
+//= require jquery
+
+//= require jquery_ujs
 //= require bootstrap-sprockets
+
+
 
 //= require_tree .
 
