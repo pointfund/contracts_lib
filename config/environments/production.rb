8bf794900ae81e46c4810f92a61f8354d2bfdb11
MX <mxselleck@gmail.com>
2018-08-03 15:26:05 -0500

    add compressor

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,6 +80,11 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+
+  
+config.assets.css_compressor = :yui
+config.assets.js_compressor = :uglifier
+
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
