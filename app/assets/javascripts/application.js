914f157e412a5726d2900045be37a92c15c946f6
MX <mxselleck@gmail.com>
2018-08-02 23:36:09 -0500

    remove bbostrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,14 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require rails-ujs
 
+//= require_self
+//= require turbolinks
+//= require activestorage
 
-//= require_tree .
-
+//= require bootstrap-sprockets
+//= require jquery
 
 
 
