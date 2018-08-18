36a72af4a37e54b3e4d720a5cd935c977370ff8b
MX <mxselleck@gmail.com>
2018-08-18 04:52:36 -0500

    add controller code add each page per action

--- app/controllers/bookbuilder_controller.rb
+++ app/controllers/bookbuilder_controller.rb
@@ -1,26 +1,74 @@
 class BookbuilderController < ApplicationController
 require 'csv'  
     def show
-        imp_page_b = "app/assets/builddb/books.csv"
+        # imp_page_b = "app/assets/builddb/books.csv"
 
-        CSV.foreach(imp_page_b, :headers => true) do |row|
-        # puts row
-        # Book.create!(row.to_hash)
-        end
     end
 
     def play
     end
 
     def build_parts
+        imp_page_p = "app/assets/builddb/page_parts.csv"
+        CSV.foreach(imp_page_p, :headers => true) do |row|
+            # puts row
+            # PagePart.create!(row.to_hash)
+        end
     end
 
     def build_layouts
+        imp_page_l = "app/assets/builddb/page_layouts.csv"
+        CSV.foreach(imp_page_l, :headers => true) do |row|
+            # puts row
+            # PageLayout.create!(row.to_hash)
+        end
     end
 
     def build_books
+        imp_page_b = "app/assets/builddb/books.csv"
+        CSV.foreach(imp_page_b, :headers => true) do |row|
+            # puts row
+            # Book.create!(row.to_hash)
+        end
     end
 
     def build_contracts
+        imp_page_c = "app/assets/builddb/contracts.csv"
+        CSV.foreach(imp_page_c, :headers => true) do |row|
+            # puts row
+            # Contract.create!(row.to_hash)
+        end 
     end
+
+
+
+       
+     
+
+
+        
+
+ 
+        
+
+
+        CSV.foreach(imp_page_d, :headers => true) do |row|
+            # puts row
+            PageLayout.create!(row.to_hash)
+        end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 end
