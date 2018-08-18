f7a27550c1e0ce6136fc55105e7c3775ad035d68
MX <mxselleck@gmail.com>
2018-08-18 06:15:08 -0500

    add books ???

--- app/controllers/bookbuilder_controller.rb
+++ app/controllers/bookbuilder_controller.rb
@@ -2,11 +2,41 @@ class BookbuilderController < ApplicationController
 require 'csv'  
     def show
         # imp_page_b = "app/assets/builddb/books.csv"
-
+        @books = Book.all
     end
 
     def play
     end
+
+
+    def build_books
+        imp_page_b = "app/assets/builddb/books.csv"
+        CSV.foreach(imp_page_b, :headers => true) do |row|
+            # puts row
+            # Book.create!(row.to_hash)
+        end
+        
+    end
+
+    def build_contracts
+        imp_page_c = "app/assets/builddb/contracts.csv"
+        CSV.foreach(imp_page_c, :headers => true) do |row|
+            # puts row
+            # Contract.create!(row.to_hash)
+        end
+        # @books = Book.all
+
+    end
+
+
+
+
+
+
+
+
+
+
 
     def build_parts
         imp_page_p = "app/assets/builddb/page_parts.csv"
@@ -14,6 +44,8 @@ require 'csv'
             # puts row
             # PagePart.create!(row.to_hash)
         end
+
+        # @contracts = Contract.all
     end
 
     def build_layouts
@@ -22,23 +54,10 @@ require 'csv'
             # puts row
             # PageLayout.create!(row.to_hash)
         end
+        
+        # @page_parts = PagePart.all
     end
 
-    def build_books
-        imp_page_b = "app/assets/builddb/books.csv"
-        CSV.foreach(imp_page_b, :headers => true) do |row|
-            # puts row
-            # Book.create!(row.to_hash)
-        end
-    end
-
-    def build_contracts
-        imp_page_c = "app/assets/builddb/contracts.csv"
-        CSV.foreach(imp_page_c, :headers => true) do |row|
-            # puts row
-            # Contract.create!(row.to_hash)
-        end 
-    end
 
 
 
