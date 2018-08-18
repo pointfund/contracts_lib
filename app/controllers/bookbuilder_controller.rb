7a4928ed2d91f26370419dddd5899726ac01c725
MX <mxselleck@gmail.com>
2018-08-18 07:12:51 -0500

    update all builder

--- app/controllers/bookbuilder_controller.rb
+++ app/controllers/bookbuilder_controller.rb
@@ -3,6 +3,7 @@ require 'csv'
     def show
         # imp_page_b = "app/assets/builddb/books.csv"
         @books = Book.all
+        @contracts = Contract.all 
     end
 
     def play
@@ -15,17 +16,19 @@ require 'csv'
             # puts row
             Book.create!(row.to_hash)
         end
-        
+        @books = Book.all
+        @contracts = Contract.all  
     end
 
     def build_contracts
         imp_page_c = "app/assets/builddb/contracts.csv"
         CSV.foreach(imp_page_c, :headers => true) do |row|
             # puts row
-            # Contract.create!(row.to_hash)
+            Contract.create!(row.to_hash)
         end
         # @books = Book.all
-
+        @contracts = Contract.all  
+        @page_parts = PagePart.all
     end
 
 
@@ -42,19 +45,20 @@ require 'csv'
         imp_page_p = "app/assets/builddb/page_parts.csv"
         CSV.foreach(imp_page_p, :headers => true) do |row|
             # puts row
-            # PagePart.create!(row.to_hash)
+            PagePart.create!(row.to_hash)
         end
-
-        # @contracts = Contract.all
+        # @contracts = Contract.all  
+        @page_parts = PagePart.all
+        @layouts = PageLayout.all
     end
 
     def build_layouts
         imp_page_l = "app/assets/builddb/page_layouts.csv"
         CSV.foreach(imp_page_l, :headers => true) do |row|
             # puts row
-            # PageLayout.create!(row.to_hash)
+            PageLayout.create!(row.to_hash)
         end
-        
+        @layouts = PageLayout.all
         # @page_parts = PagePart.all
     end
 
