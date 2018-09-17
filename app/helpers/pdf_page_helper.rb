d3c65de77a1c167b218de75f951ed1ce747dd231
MX <mxselleck@gmail.com>
2018-09-17 18:49:16 -0500

    one time last 2 pages

--- app/helpers/pdf_page_helper.rb
+++ app/helpers/pdf_page_helper.rb
@@ -8150,13 +8150,13 @@ module PdfPageHelper
 			# # # ##########################################################################################
 			pdf.start_new_page
 
-			# pdf.bounding_box([-30, 680], :width => 100, :height => 100, :at => [200, 550]) do
-			#           pdf.fill_color "000000"
-			# 	pdf.transparent(1.0, 0.2) do 
-			# 		pdf.image open("app/assets/page_files/con_9_page_12.png"), :fit => [600, 1000], :at => [0, 160]
-			# 	end
-			#          pdf.fill_color "000000"
-			# end
+			pdf.bounding_box([-30, 680], :width => 100, :height => 100, :at => [200, 550]) do
+			          pdf.fill_color "000000"
+				pdf.transparent(1.0, 0.2) do 
+					pdf.image open("app/assets/page_files/con_9_page_12.png"), :fit => [600, 1000], :at => [0, 160]
+				end
+			         pdf.fill_color "000000"
+			end
 
 
 			# # # page 13
