8fb966b9fe92c33628149cb4c3dbb3dadb77ae2c
MX <mxselleck@gmail.com>
2018-09-26 13:10:34 -0500

    update remove page 8 - 9 update

--- app/controllers/bookbuilder_controller.rb
+++ app/controllers/bookbuilder_controller.rb
@@ -53,7 +53,7 @@ class BookbuilderController < ApplicationController
         @layouts = PageLayout.all
         # PagePart.all
         imp_page_b = "app/assets/builddb/page_parts_main.csv"
-        imp_page_p = "app/assets/builddb/page_parts_3.csv"
+        # imp_page_p = "app/assets/builddb/page_parts_3.csv"
 
     if(@page_parts.length <= 0)
         CSV.foreach(imp_page_b, :headers => true) do |row|
@@ -78,7 +78,7 @@ class BookbuilderController < ApplicationController
         @layouts = PageLayout.all
 
         imp_page_d = "app/assets/builddb/page_layouts_main.csv"
-        imp_page_lay = "app/assets/builddb/page_layouts_3.csv"
+        # imp_page_lay = "app/assets/builddb/page_layouts_3.csv"
         if(@layouts.length <= 0)
             CSV.foreach(imp_page_d, :headers => true) do |row|
                 puts row
@@ -91,56 +91,56 @@ class BookbuilderController < ApplicationController
         end
     #     @layouts = PageLayout.all
     #     @page_parts = PagePart.all
-        build_page_09
+        # build_page_09
     end
 
 
-    def build_page_09
-        @books = Book.all
-        @contracts = Contract.all
-        @page_parts = PagePart.all
-        @layouts = PageLayout.all
-
-        imp_page_09 = "app/assets/builddb/page_parts_09.csv"
-        imp_lay_09 = "app/assets/builddb/page_layout_09.csv"
-        if(@layouts.length <= 193)
-            CSV.foreach(imp_page_09, :headers => true) do |row|
-                # puts row
-                # PagePart.create!(row.to_hash)
-            end
-            CSV.foreach(imp_lay_09, :headers => true) do |row|
-                # puts row
-                # PageLayout.create!(row.to_hash)
-            end
-        end
-    #     @layouts = PageLayout.all
+    # def build_page_09
+    #     @books = Book.all
+    #     @contracts = Contract.all
     #     @page_parts = PagePart.all
-    end     
+    #     @layouts = PageLayout.all
+
+    #     imp_page_09 = "app/assets/builddb/page_parts_09.csv"
+    #     imp_lay_09 = "app/assets/builddb/page_layout_09.csv"
+    #     if(@layouts.length <= 193)
+    #         CSV.foreach(imp_page_09, :headers => true) do |row|
+    #             # puts row
+    #             # PagePart.create!(row.to_hash)
+    #         end
+    #         CSV.foreach(imp_lay_09, :headers => true) do |row|
+    #             # puts row
+    #             # PageLayout.create!(row.to_hash)
+    #         end
+    #     end
+    # #     @layouts = PageLayout.all
+    # #     @page_parts = PagePart.all
+    # end     
 
 
         
 
-    def build_page_08
-        @books = Book.all
-        @contracts = Contract.all
-        @page_parts = PagePart.all
-        @layouts = PageLayout.all
-
-        imp_page_08 = "app/assets/builddb/page_parts_08.csv"
-        imp_lay_08 = "app/assets/builddb/page_layout_08.csv"
-        if(@layouts.length <= 380)
-            CSV.foreach(imp_page_08, :headers => true) do |row|
-                # puts row
-                PagePart.create!(row.to_hash)
-            end
-            CSV.foreach(imp_lay_08, :headers => true) do |row|
-                # puts row
-                # PageLayout.create!(row.to_hash)
-            end
-        end
-    #     @layouts = PageLayout.all
+    # def build_page_08
+    #     @books = Book.all
+    #     @contracts = Contract.all
     #     @page_parts = PagePart.all
-    end     
+    #     @layouts = PageLayout.all
+
+    #     imp_page_08 = "app/assets/builddb/page_parts_08.csv"
+    #     imp_lay_08 = "app/assets/builddb/page_layout_08.csv"
+    #     if(@layouts.length <= 380)
+    #         CSV.foreach(imp_page_08, :headers => true) do |row|
+    #             # puts row
+    #             PagePart.create!(row.to_hash)
+    #         end
+    #         CSV.foreach(imp_lay_08, :headers => true) do |row|
+    #             # puts row
+    #             # PageLayout.create!(row.to_hash)
+    #         end
+    #     end
+    # #     @layouts = PageLayout.all
+    # #     @page_parts = PagePart.all
+    # end     
 
 
 
